fef297443f0293aaf5509a2f718e5042f5c76720
alvarani <alva.rani@scilifelab.se>
2012-06-20 11:13:58 +0200

    the edited tophat.pl file

--- map/tophat.singleend.gensbatch.pl
+++ map/tophat.singleend.gensbatch.pl
@@ -5,36 +5,36 @@ use File::Basename;
 use File::Spec::Functions;
 
 
-my ($sample, $fastqprefix, $fastqsuffix, $outdir, $threads, $gtf, $ref, $isizefile, $ods, $projid, $email, $time, $readlen, $isizedev) = @ARGV;
+my ($fastqprefix, $outdir, $threads, $gtf, $ref,$ods, $projid, $email, $time, $readlen) = @ARGV;
 
 #set vars
-my $fastqfile_1 = $fastqprefix . '1' . $fastqsuffix;
+my $fastqfile_1 = $fastqprefix . '1';
 #my $fastqfile_2 = $fastqprefix . '2' . $fastqsuffix;
 my $fastqprefix_base = basename($fastqprefix);
 my $outdir = catfile($outdir, $fastqprefix_base);
 $outdir =~ s/\.$//;
 
 #set sbatch file
-my $sbatch_file = catfile($ods, $fastqprefix_base . $fastqsuffix . '.tophat.sbatch');
+my $sbatch_file = catfile($ods, $fastqprefix_base . '.tophat.sbatch');
 $sbatch_file =~ s/\.\./\./;
 
 #set insert size (NB: actually inner distance)
 my $isize;
 open(ISIZE, '<', $isizefile) or die("Couldnt open $isizefile, $!\n");
-while(defined(my $line = <ISIZE>)){
-  chomp($line);
+#while(defined(my $line = <ISIZE>)){
+#  chomp($line);
 #  print 'DEBUG: ' . $line . "\n";
-  my ($jsample, $isize_jsample) = split(/\t/, $line);
+ # my ($jsample, $isize_jsample) = split(/\t/, $line);
 #  print 'DEBUG: jsample: ' . $jsample . "\n";
 #  print 'DEBUG: sample: ' . $sample . "\n";
 #  my ($isize_jsample, $isize_jsample, $isizedev_jsample) = split(/ /, $line);
-  if($jsample =~ $sample){
-    $isize = $isize_jsample;
-    $isize = $isize - 2 * $readlen; #Tophat uses the inner distance: isize - 2*readlen
+  #if($jsample =~ $sample){
+   # $isize = $isize_jsample;
+   # $isize = $isize - 2 * $readlen; #Tophat uses the inner distance: isize - 2*readlen
 #    $isizedev = $isizedev_jsample;
-  }
-}
-close(ISIZE);
+  #}
+#}
+#close(ISIZE);
 
 #create outdirs
 my $cmd = 'mkdir -p ' . catfile($outdir, 'log');
@@ -62,4 +62,5 @@ print SBATCH 'module load tophat/1.4.0' . "\n";
 #print cmd
 print SBATCH 'tophat --solexa1.3-quals -p ' . $threads . ' -o ' . $outdir . ' --GTF ' . $gtf . " -r '" . $isize . "' --mate-std-dev " . $isizedev . ' ' . $ref . ' ' . $fastqfile_1 . ' ' . "\n";
 close(SBATCH);
+
 

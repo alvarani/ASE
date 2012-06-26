aebd16161fb33832f06aaca4fe2387de4683c6cf
alvarani <alva.rani@scilifelab.se>
2012-06-26 16:27:55 +0200

    The  tophat.sh for single end read  file

--- map/run.tophat.sh
+++ map/run.tophat.sh
@@ -9,38 +9,57 @@
 #Longest run: 5h38min, 6G file for each readpair: 8_LPS.readp_1.2.ill.fastq
 email='alva.rani@scilifelab.se'
 projid='b2012046'
-execdir='/bubo/home/h24/alvaj/glob/code/ASE/mont'
-fastqdir='/proj/b2012046/edsgard/ase/sim/data/degner'
-fastqsuffix='.fastq'
+#execdir='/bubo/home/h24/alvaj/glob/code/ASE/map'
+#fastqdir='/proj/b2012046/edsgard/ase/sim/data/degner'
+fastqdir='/proj/b2012046/rani/data/fastq'
+fastqfile='testsingle.bqB.fastq'
 outdir='/proj/b2012046/rani/analysis/monte'
 sbatchdir='/proj/b2012046/rani/scripts/monte'
 
 #Vars.
-threads='8'
+#threads='8'
 annotdir='/bubo/home/h26/edsgard/glob/annotation/human/'
-gtf=${annotdir}'Homo_sapiens.GRCh37.59.gtf'
+gtf=${annotdir}/'Homo_sapiens.GRCh37.59.gtf'
 ref='/bubo/home/h26/edsgard/glob/annotation/bowtie/concat.fa'
-time='23:00:00' #Longest run: 7h10min: Input: ~7.3G per readpair fastq file
-readlen='100'
-isizefile='/proj/b2011075/data/rnaseq/bioanalyzer/isizes.only.tab'
-isizedev='75' #very rough approx from the bioanalyzer plots. Also, we use trimming which further adds to the deviation.
+#time='01:00:00'
 
+#Longest run: 7h10min: Input: ~7.3G per readpair fastq file
+
+#readlen='100'
+#isize='175'
+#isizedev='75' 
+
+#very rough approx from the bioanalyzer plots. Also, we use trimming which further adds to the deviation.
 
 cd $sbatchdir
 #rm cmds.sh
-samples=(`cat samples.list`)
-for sample in ${samples[@]}
-do
-    find $fastqdir -maxdepth 1 -name ${sample}'*' | sed 's/[12].filter.fastq//' | grep -v '.S.filter.fastq' | sort -u >fastqfiles.prefix.list
-    cat fastqfiles.prefix.list | xargs -I% echo perl ${execdir}/'tophat.singleend.gensbatch.pl' $sample % $fastqsuffix $outdir $threads $gtf $ref $isizefile $sbatchdir $projid $email $time $readlen $isizedev >>cmds.sh
-done
-sh cmds.sh
-find . -name '*.tophat.sbatch' | xargs -I% sbatch %
-#Check:
-#Same params in sbatch files that were run for real data:
-#cat /proj/b2011075/analysis/hs_pipe/outscripts/run1/1_unstim_12h/tophat/tophat_1_unstim_12h.lane_1.index_8.readp_1.fastq.1.filter.fastq.sbatch
+#samples=(`cat samples.list`)
 
-#Check:
-find . -name 'tophat*.stderr' | xargs -I% tail -1 % | sort
+#for sample in ${samples[@]}
+#do
+#find $fastqdir -maxdepth 1 -name ${samples}'*' | sort -u >fastqfiles.list
+
+(cat <<EOF
+#!/bin/bash -l
+#SBATCH -A b2012046
+#SBATCH -t 01:00:00
+#SBATCH -J tophat
+#SBATCH -p node -n 8
+#SBATCH -e /proj/b2012046/rani/analysis/monte/test4single/log/tophat.jid_%j.stderr
+#SBATCH -o /proj/b2012046/rani/analysis/monte/test4single/log/tophat.jid_%j.stdout
+#SBATCH --mail-type=All
+#SBATCH --mail-user=alva.rani@scilifelab.se
+module load bioinfo-tools
+module load samtools/0.1.18
+module load tophat/1.4.0
+tophat --solexa1.3-quals -G $gtf -o  $outdir $ref ${fastqdir}/testsingle.bqB.fastq >${outdir}/.tophat.sbatch
+EOF
+) > sbatch.tophat
+
+
+
+find . -name 'sbatch.tophat' | xargs -I% sbatch %
+
+
 
 #submitted

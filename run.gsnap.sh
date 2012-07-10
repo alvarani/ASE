a4e7fd0598ad6fc41a7bf77d0e0fc0df6cdfb4c2
alvarani <alva.rani@scilifelab.se>
2012-07-10 18:54:02 +0200

    GSNAP for the small sized fastq files

--- run.gsnap.sh
+++ run.gsnap.sh
@@ -40,16 +40,15 @@ cat splicesiteschr | ${gsnapexecdir}/iit_store -o splicesiteschro
 ## done
 
 ###
-## Fastq files
+## Run gsnap
 ###
 
 #set path for for each of these variables 
-gsnapexecdir='/bubo/home/h24/alvaj/opt0/gmap-2012-06-06'
+gsnapexecdir='/bubo/home/h24/alvaj/opt0/gmap-2012-06-12'
 annotdir='/bubo/home/h24/alvaj/glob/annotation/gsnap'
-scriptdir='/proj/b2012046/rani/scripts/gsnap'
-fastqdir='/proj/b2012046/edsgard/ase/sim/data/synt/fastqfilt'
+scriptdir='/proj/b2012046/rani/scripts/gsnap/test'
+fastqdir='/proj/b2012046/rani/data/synt/fastqfilt/oneGchunks'
 outdir='/proj/b2012046/rani/analysis/gsnap'
-
 splicefile=${annotdir}/'splicesiteschro'
 refdir=${annotdir}/gmapdb
 ref=hg19
@@ -58,35 +57,47 @@ snpfile=dbsnp135
 projid='b2012046'
 email='alva.rani@scilifelab.se'
 
+#Rename fastq files
+cd $fastqdir
+ln -s /proj/b2012046/edsgard/ase/sim/data/synt/fastqfilt/oneGchunks/*.fastq .
+#append readp_1 as suffix
+ls -1 *.readp_1.*.fastq | xargs -I% mv % %.readp_1
+#rm first occurence of readp_1
+rename .readp_1.part .part *.readp_1
+#append readp_2 as suffix
+ls -1 *.readp_2.*.fastq | xargs -I% mv % %.readp_2
+#rm first occurence of readp_2
+rename .readp_2.part .part *.readp_2
 
-# all files apart from those with '.S' extension
 cd $scriptdir
-find $fastqdir -maxdepth 1 -name '*fastq' | grep -v '\.S\.' >fastq.files
+find $fastqdir -maxdepth 1 -name '*fastq.readp_1' | sed 's/.readp_1//' >fastq.files.prefix
 
-#Create sbatch scripts
-cat fastq.files | sed 's/1.filter.fastq//' | grep -v '2.filter.fastq' >fastqfiles.prefix
+samples=(`cat fastq.files.prefix`)
 
+for fastqfile in ${samples[@]}
+do
+sbatchfile=`basename $fastqfile`
+echo $sbatchfile
 (cat <<EOF
 #!/bin/bash -l
 #SBATCH -A $projid
-#SBATCH -t 35:00:00
+#SBATCH -t 30:00:00
 #SBATCH -J gsnap
 #SBATCH -p node -n 1
-#SBATCH -e $outdir/log/gsnap.samplejid_%j.stderr
-#SBATCH -o $outdir/log/gsnap.samplejid_%j.stdout
+#SBATCH -e $outdir/log/gsnap.${sbatchfile}.jid_%j.stderr
+#SBATCH -o $outdir/log/gsnap.${ssbatchfile}.jid_%j.stdout
 #SBATCH --mail-type=All
 #SBATCH --mail-user=$email
 export PATH=$PATH:${gsnapexecdir}/bin
 cd ${fastqdir}
 module load bioinfo-tools
 module load samtools/0.1.18
-gsnap -D $refdir -d $ref -A sam -s $splicefile -V $snpdir -v $snpfile --quality-protocol=illumina sample1.filter.fastq sample2.filter.fastq | samtools view -Sb -  >${outdir}/samplesam
+gsnap -D $refdir -d $ref -A sam -s $splicefile -V $snpdir -v $snpfile --quality-protocol=illumina ${fastqfile}.readp_1 ${fastqfile}.readp_2 | samtools view -Sb - >${outdir}/${sbatchfile}.bam
 EOF
-) >sbatch.template
-cat fastqfiles.prefix | xargs -I% basename % | xargs -I% echo cat sbatch.template "| sed 's/sample/"%"/g' >" %gsnap.sbatch >cmds.sh
-sh cmds.sh
-find . -name '*.gsnap.sbatch' | xargs -I% sbatch %
-
+)>${sbatchfile}.sbatch
+done
+find . -name '*.fastq.sbatch' | xargs -I% sbatch %
+#status: submitted
 
 
 

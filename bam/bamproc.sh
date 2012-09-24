af1a843c20a951fecf4ff5ba747c4850280c9493
alvarani <alva.rani@scilifelab.se>
2012-09-24 17:02:00 +0200

    The new BAmproc with Minor changes

--- bam/bamproc.sh
+++ bam/bamproc.sh
@@ -9,13 +9,14 @@
 email='alva.rani@scilifelab.se'
 projid='b2012046'
 execdir='/bubo/home/h24/alvaj/glob/code/ASE/bam'
-sbatchdir='/proj/b2012046/rani/scripts/gsnap/bamscripts/'
-bamfile='/proj/b2012046/rani/analysis/gsnap/'
+sbatchdir='/proj/b2012046/rani/scripts/gsnap/unmapped'
+bamfile='/proj/b2012046/rani/analysis/gsnap/bamsort/'
 cd $sbatchdir
-find $bamfile -name '*.bam' | awk -F'/' -v execdir=$execdir -v sbatchdir=$sbatchdir -v projid=$projid -v email=$email '{print "perl", execdir"/rmdups.pl", $0, $NF, sbatchdir, projid, email;}' >rmdups.sh
+find $bamfile -name '*.uniqmapped.bam' | awk -F'/' -v execdir=$execdir -v sbatchdir=$sbatchdir -v projid=$projid -v email=$email '{print "perl", execdir"/rmdups.pl", $0, $NF, sbatchdir, projid, email;}' >rmdups.sh
 sh rmdups.sh
 find . -name '*.sbatch' | xargs -I% sbatch %
-#### 
+####
+
 
 #Check:
 find $bamfile -name '*.bam.sorted.nodup' | xargs -I% ls -lrth %
@@ -26,7 +27,12 @@ find $bamfile -name '*.rmdups.stderr' | xargs -I% grep -i 'warn'
 find $bamfile -name '*.bam'  | xargs -I% rm %
 
 #rename bamfiles
-find $bamfile -name '*.bam.sorted' | xargs -I% echo rename '.bam.sorted' '.sorted.bam' % >cmds.sh
+find  -name '*.bam.sorted' | xargs -I% echo rename '.bam.sorted' '.sorted.bam' % >cmds.sh
+sh cmds.sh
+
+
+#rename bamfiles
+find $bamdir -name '*.bam.sorted.nodup' | xargs -I% echo rename '.bam.sorted.nodup' '.sorted.nodup.bam' % >cmds.sh
 sh cmds.sh
 ####
 
@@ -37,15 +43,17 @@ projid='b2012046'
 email='alva.rani@scilifelab.se'
 sbatchdir='/proj/b2012046/rani/scripts/gsnap/mergebams'
 execdir='/bubo/home/h24/alvaj/glob/code/ASE/bam'
-bamfile='/proj/b2012046/rani/analysis/gsnap/'
-outdir=$bamfile
-time='1:00:00'
+bamsort='/proj/b2012046/rani/analysis/gsnap/bamsort'
+outdir='/proj/b2012046/rani/analysis/gsnap/mergebam'
+time='11:00:00'
 cd $sbatchdir
-find $bamfile -name '*.sorted.bam' | sed 's/\..*//' | sort -u >samples.list
-cat samples.list | xargs -I% basename % | xargs -I% echo perl ${execdir}'/mergebams.pl' % $bamfile $outdir $sbatchdir $projid $email $time >cmds.sh
+find $bamsort -name '*.sorted.nodup.bam' | sed 's/\..*//' | sort -u >samples.list
+cat samples.list | xargs -I% basename % | xargs -I% echo perl ${execdir}'/mergebams.pl' % $bamsort $outdir $sbatchdir $projid $email $time >cmds.sh
 sh cmds.sh
-find $sbatchdir -name '*.sbatch' | xargs -I% sbatch %
+find $sbatchdir -name '*.mergesams.sbatch' | xargs -I% sbatch %
 
+
+## Submitted
 #Check:
 find ${bamfile}/info/*.stderr | xargs -I% grep -i 'err' %
 find ${bamfile}/info/*.mergesams.*.stderr | xargs -I% grep -i 'warn' %
@@ -54,16 +62,17 @@ find ${bamfile}/info/*.mergesams.*.stderr | xargs -I% grep -i 'warn' %
 ###
 #Create indexes
 ###
-find $bamfile -maxdepth 1 -name '*.bam' | xargs -I% echo samtools index % > cmd.sh
+bamsort='/proj/b2012046/rani/analysis/gsnap/mergebam'
+find $bamsort -maxdepth 1 -name '*.bam' | xargs -I% echo samtools index % > cmd.sh
 #Manually add sbatch header to the cmds.sh script
 (cat <<EOF
 #!/bin/bash -l
 #SBATCH -A $projid
-#SBATCH -t 2:00:00
+#SBATCH -t 3:00:00
 #SBATCH -J indexbam
 #SBATCH -p core -n 1
-#SBATCH -e ${bamfile}/info/indexbam.jid_%j.stderr
-#SBATCH -o ${bamfile}/info/indexbam.jid_%j.stdout
+#SBATCH -e ${bamsort}/info/indexbam.jid_%j.stderr
+#SBATCH -o ${bamsort}/info/indexbam.jid_%j.stdout
 #SBATCH --mail-type=All
 #SBATCH --mail-user=$email
 module load bioinfo-tools
@@ -72,3 +81,7 @@ EOF
 ) >bamindex.sbatchheader
 cat bamindex.sbatchheader cmd.sh >bamindex.sbatch
 sbatch bamindex.sbatch
+
+############################
+## Done all steps here without errors
+

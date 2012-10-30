e463b7a96bdeb7a0a411903525ac963b43924d69
alvarani <alva.rani@scilifelab.se>
2012-10-30 16:33:29 +0100

    The new bamerr.run.sh

--- GSNAP/bam/bamerror.run.sh
+++ GSNAP/bam/bamerror.run.sh
@@ -1,37 +1,71 @@
-
+#DEP : addhead.pl (Certain bam files formed without header so added bthe SAM header manually)
+#DEP : rm.multimappedreads.pl
 ###Test for 19 sam file
 #created a errorlist of 19 files
-# taking the the reads appreaed more than twice and taking it in a order 
-cat errorfiles.list | xargs -I% echo "samtools view "%" | awk '{print \$1;}' | sort | uniq -c >"%.reads2nmapped >cmds.sh
-
-
+# taking the the reads appeared more than twice and taking it in a order 
 
 errordir='/proj/b2012046/rani/analysis/gsnap/bamsort/errortest'
+cd $errordir
+cat errorlist | xargs -I% echo "/proj/b2012046/rani/analysis/gsnap/bam/"%".fastq.bam " >errorbamfiles.list 
+cat  errorbamfiles.list | xargs -I% echo "samtools view "%" | awk '{print \$1;}' | sort | uniq -c >"%.reads2nmapped >cmds.sh
+sh cmds.sh
 
-ls -1 ${errordir}/*.reads2nmapped >reads2nmapped.files
-
+# taking out the number of uniquely mapped reads and non uniqly mapped reads
 cat reads2nmapped.files | xargs -I% basename % | xargs -I% echo "awk '\$1 == 2 {print \$0;}'" ${errordir}/% " | wc -l >" %.n.uniq >cmds.sh
+sh cmds.sh
 cat reads2nmapped.files | xargs -I% basename % | xargs -I% echo "awk '\$1 != 2 {print \$0;}'" ${errordir}/% " | wc -l >" %.n.nonuniq >cmds2.sh
+sh cmds2.sh
 
-
-bamfiles='/proj/b2012046/rani/analysis/gsnap/bamsort/errortest/errorbamfiles.list'
-errordir='/proj/b2012046/rani/analysis/gsnap/bamsort/errortest'
 
 #Extract multimapped reads
 ls -1 ${errordir}/*.reads2nmapped >reads2nmapped.files
 cat reads2nmapped.files | xargs -I% basename % | xargs -I% echo "awk '\$1 != 2 {print \$0;}'" ${errordir}/% "| awk '{print \$2;}'>" %.nonuniq >cmds.sh
 sh cmds.sh &
 
-#TBD: Convert all bams to sams
-bamdir='/proj/b2012046/rani/analysis/gsnap/bamindex'
-sam=${bamdir}/2_LPS.part_2ab.sam
+# Convert all bams to sams
+bamdir='/proj/b2012046/rani/analysis/gsnap/bam'
+sam=${errordir}
+module load bioinfo-tools
+module load samtools/0.1.18
+cat  errorbamfiles.list | xargs -I% echo "samtools view -h -o /proj/b2012046/rani/analysis/gsnap/bamsort/errortest/"%".sam  "%".bam" >cmd.sh
+(cat <<EOF
+#!/bin/bash -l
+#SBATCH -A b2012046
+#SBATCH -t 6:00:00
+#SBATCH -J bamtosam
+#SBATCH -p node -n 2
+#SBATCH -e /proj/b2012046/rani/analysis/gsnap/bamsort/errortest/log/bamtosam.stderr
+#SBATCH -o /proj/b2012046/rani/analysis/gsnap/bamsort/errortest/log/bamtosam.stdout
+#SBATCH --mail-type=All
+#SBATCH --mail-user=alva.rani@scilifelab.se
+sh cmd.sh
+EOF
+)>${errordir}
 
 #Rm multimapped reads
 ls -1 *d.nonuniq | awk -F'.' -v OFS='.' '{print $1, $2;}' >errorfiles.prefix.list
 cat errorfiles.prefix.list | xargs -I% echo "perl rm.multimappedreads.pl " %.sam %.fastq.bam.reads2nmapped.nonuniq ">"%.uniqmapped.sam >cmds.sh
 
-## How to add a word or letter to the first column of the file
- cat CCDS-hg19.chrstripped.bed | awk -Fchr '{print "chr"$1"";}' >CCDS-hg19.chrstripped.Nbed
-## How to remove lines from 1-24 in afile
-cat hg19.chrsizes.chrstripped | sed '1,24d' >body.chrstripped
+# Convert all bams to sams
+module load bioinfo-tools
+module load samtools/0.1.18
+ls  -1 *.uniqmapped.sam | xargs -I% echo  "samtools view -Sb -" >"%".bam >cmds.sh
+(cat <<EOF
+#!/bin/bash -l
+#SBATCH -A b2012046
+#SBATCH -t 6:00:00
+#SBATCH -J samtobam
+#SBATCH -p node -n 2
+#SBATCH -e /proj/b2012046/rani/analysis/gsnap/bamsort/errortest/log/bamtosam.stderr
+#SBATCH -o /proj/b2012046/rani/analysis/gsnap/bamsort/errortest/log/bamtosam.stdout
+#SBATCH --mail-type=All
+#SBATCH --mail-user=alva.rani@scilifelab.se
+sh cmds.sh
+EOF
+)>${errordir}
+
+# Additional steps
+# Certain samples did'nt contain the header after the emoval of multi-mapped reads, for that sample I added header manually by
+perl addheader.pl header 'The sample without header'
+
 
